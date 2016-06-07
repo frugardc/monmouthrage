e14c7b1e75515e7ae9ec0a88d9f9c05aba334532
Christian Frugard <cfrugard@lightower.com>
2016-06-07 12:48:32 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rage::Application.config.secret_token = 'b1ec63b3627a7f4b6079a0286f5b062855593886462c119aa047d5c2734c6e675ea18d1e13e4df89580487c2a80d93e3163344e8165beaac96c02dde3e26b772'
