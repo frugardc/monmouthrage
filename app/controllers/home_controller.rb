d63607d9f5ed34f8f08c21aaf79a35384be99bd0
Christian Frugard <cfrugard@lightower.com>
2016-06-07 13:54:52 -0400

    adding heroku database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @address = params[:address]
+    @geocodes = Geocoder.search(@address)
+    @result = @geocodes.first
+
+    respond_to do |format|
+      format.html
+    end
   end
 end
