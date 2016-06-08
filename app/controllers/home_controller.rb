e14f24bd213bcc6a438898c14b5774f10411eabe
Christian Frugard <cfrugard@lightower.com>
2016-06-08 16:51:08 -0400

    getting strings from database because heroku doesnt hendle geoms well

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
       @power_line_string = @power_line_data[0]
       @power_line_distance = @power_line_data[1]
     end
-    @schools_data = School.all.collect{|school| [school,@power_line.to_distance_placemark(school.geom.first.latitude,school.geom.first.longitude)]}
+    @schools_data = School.all.collect{|school| [school,@power_line.to_distance_placemark(school.location.latitude,school.location.longitude)]}
     respond_to do |format|
       format.html
     end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,10 @@
 class School < ActiveRecord::Base
   GEO_FACTORY = RGeo::Geographic.spherical_factory(srid: 4326)
   set_rgeo_factory_for_column :geom, GEO_FACTORY
-
+  LOCATION_STRUCT = Struct.new(:latitude,:longitude)
+  default_scope :select => "*,st_astext(geom) as wkt"
+  def location
+    parts = wkt.gsub(/[A-Z()]/i,"").split(" ")
+    LOCATION_STRUCT.new(parts[1].to_f,parts[0].to_f)
+  end
 end
